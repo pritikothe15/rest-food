38271cce4e81fc0a90c699fd1f0025996f2d1593
Priti Kothe <pritikothe15@gmail.com>
2023-01-30 18:24:20 +0530

    feat: login and current user storage

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { currentUser } from "../../util/currentUser";
 
 function Home() {
+  
   return (
-    <div>Home component</div>
+    <div>
+      <h1 className='text-center'>Home</h1>
+      <h2> {currentUser?.name}</h2>
+    </div>
   )
 }
 
